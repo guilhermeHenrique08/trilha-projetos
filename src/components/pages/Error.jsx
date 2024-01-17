2a2026a4ef38b2c5a14d16550a5bc9494a24e962
Guilherme <guilhermehenrique12308@gmail.com>
2024-01-17 09:14:38 -0300

    add page error

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -1,7 +1,24 @@
-const Error = () => {
+import { FaRegSadTear } from "react-icons/fa";
+
+import LinkButton from "../layout/LinkButton";
+
+function Error() {
   return (
-    <div>Error</div>
-  )
+    <div className="mx-auto h-screen flex flex-col items-center justify-center">
+      <div className="bg-white flex flex-col gap-4 p-5 max-w-lg">
+        <div className="flex items-center gap-5">
+          <FaRegSadTear className="text-xl text-black"/>
+          <h1 className="text-xl font-bold text-black">Pagina não encontrada</h1>
+        </div>
+        <p className="text-gray-500">
+          Ah, não conseguimos encontrar a página que você está procurando. Tente
+          voltar à página anterior ou apenas clique no botão abaixo para voltar
+          a pagina inicial
+        </p>
+        <LinkButton to="/" text="Voltar a pagina inicial" />
+      </div>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
